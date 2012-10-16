b11ecdc8622897236d5266100cc6501158d83685
Anton Lindström <carlantonlindstrom@gmail.com>
2012-10-16 12:10:39 +0200

    meee

---
+++ Factorizer/Factorizer/factorizer.h
@@ -0,0 +1,8 @@
+#ifndef FACTORIZER_H
+#define FACTORIZER_H
+
+void f(mpz_t x, mpz_t number);
+void find_perfect_power(mpz_t base, unsigned long * exp, mpz_t number);
+void pollardsRoh(mpz_t number, mpz_t d);
+
+#endif
